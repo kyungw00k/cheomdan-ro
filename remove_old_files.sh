47ea315ab14d7a080211fa8a8135fddfe79af55e
Kyungwook Park <parksama@gmail.com>
2021-12-07 11:13:37 +0900

    use mmin instead of mtime

--- remove_old_files.sh
+++ remove_old_files.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/bin/find /home/pi/cheomdan-ro/docs/snapshots -type f -mtime +3 -exec rm {} +
+/usr/bin/find /home/pi/cheomdan-ro/docs/snapshots -type f -mmin +30 -exec rm {} +
 /usr/bin/find /home/pi/cheomdan-ro/logs -type f -mtime +3 -exec rm {} +
